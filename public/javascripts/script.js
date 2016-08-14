af99cc75e6c657a9d73b8046e4b18db8865e8e07
wtser <wtser@segmentfault.com>
2016-08-15 00:42:59 +0800

    add track

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,6 +1,13 @@
 window.feed = {};
 window.articleLoading = false;
 
+var APP_ID = '0njXGCBddbM2eUuCwxvbUq9t-gzGzoHsz';
+var APP_KEY = 'fTETgbsVeJA0qFnaB3MX6i6x';
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+});
+
 function renderArticles() {
     articleLoading = true;
     $.post("/news", {feed: JSON.stringify(feed)}, function (data) {
@@ -34,6 +41,34 @@ $(".reader__site-item--sites").on("click", ".reader__site-item", function () {
 
 $(".reader__site-item--all").on("click",function(){
     $(".reader__site-items").toggle()
+})
+
+$(".reader__list").on("click",".reader__list-item",function(){
+    var that = $(this);
+    var article = {
+        title:that.find('.reader__list-item-title').text(),
+        url:that.find('.reader__list-item-below').text()
+    }
+
+    var query = new AV.Query('Article');
+    query.equalTo('title', article.title);
+    query.find().then(function (rows) {
+        if (rows.length) {
+
+        } else {
+
+            var Article = AV.Object.extend('Article');
+            // 新建对象
+            var articleObj = new Article();
+
+            articleObj.save(article)
+
+        }
+
+    }, function (err) {
+        console.log(err)
+
+    })
 })
 
 $(window).scroll(function () {
